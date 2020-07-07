8e2c686d49e52501eb0f3d207dd3e18124e11db5
rookie0x17 <daniel.gavrila17@gmail.com>
2020-07-07 11:24:01 +0200

    varie feature

--- functions.php
+++ functions.php
@@ -1,17 +1,42 @@
 <?php
 
-session_start();
-$_SESSION['id'] = isset( $_SESSION['id'] )? $_SESSION['id'] : '';
+    session_start();
+    $_SESSION['id'] = isset( $_SESSION['id'] )? $_SESSION['id'] : '';
 
-$link = mysqli_connect("localhost" , "daniel" , "museum" , "int-museum");
+    $link = mysqli_connect("localhost" , "daniel" , "museum" , "int-museum");
 
-if (mysqli_connect_errno()) {
+    if (mysqli_connect_errno()) {
 
-    print_r(mysqli_connect_error());
-    exit();
-}
+        print_r(mysqli_connect_error());
+        exit();
+    }
 
-if (isset($_GET['function']) && $_GET['function']== "logout"){
-    session_unset();
-}
+    if (isset($_GET['function']) && $_GET['function']== "logout"){
+        session_unset();
+    }
+
+    function displayRecentArtwork(){
+
+        global $link;
+
+        $query = "SELECT * FROM artwork ORDER BY `date` DESC LIMIT 10 ";
+        
+        $result = mysqli_query($link , $query);
+
+        if(mysqli_num_rows($result) == 0 ){
+
+            echo "There are no artwork";
+
+        }
+
+        else {
+
+            while ($row = mysqli_fetch_assoc($result)) {
+
+                echo "<p class='artworkName'>". $row['name'] ." <span class='time' > " . $row['date'] . "</p>" ;
+            }
+
+        }
+
+    }
 ?>
--- views/home.php
+++ views/home.php
@@ -1,6 +1,16 @@
 
-<div class="container">
+<div class="mainContainer">
 
-<h1>Hello, world!</h1>
+    <div class="row">
+        <div class="col-md-8">
+            <h1>What is Interactive Classical Art?</h1>
+        </div>
+        <div class="col-md-4" id="recArtwork">
+            <h2>Recent artworks added</h2>
+            
+            <?php displayRecentArtwork(); ?>
+        </div>
+
+    </div>
 
 </div>
