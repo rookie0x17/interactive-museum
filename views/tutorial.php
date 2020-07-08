d96876d3d520db603bd170a098a6eb4a6adefaa1
alessandromigliore <62027392+alessandromigliore@users.noreply.github.com>
2020-07-08 12:34:12 +0200

    Update tutorial.php

--- views/tutorial.php
+++ views/tutorial.php
@@ -24,14 +24,14 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="https://localhost/interactive2/index.php">Home</a>
+        <a class="nav-link" href="https://localhost/interactive-museum/index.php">Home</a>
       </li>
       
       <li class="nav-item">
         <a class="nav-link" href="?page=artwork"> Artwork </a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="https://localhost/interactive2/views/tutorial.php">Tutorial<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="https://localhost/interactive-museum/views/tutorial.php">Tutorial<span class="sr-only">(current)</span></a>
       </li>
       
     </ul>
