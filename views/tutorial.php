1af1f4671d2a704b959049e59353f1112e039d78
alessandromigliore <62027392+alessandromigliore@users.noreply.github.com>
2020-07-08 12:33:28 +0200

    Add files via upload

---
+++ views/tutorial.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Tutorial</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="" style="font-style:italic">Interactive Art Museum</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="https://localhost/interactive2/index.php">Home</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="?page=artwork"> Artwork </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="https://localhost/interactive2/views/tutorial.php">Tutorial<span class="sr-only">(current)</span></a>
+      </li>
+      
+    </ul>
+  </div>
+   <form class="form-inline pull-xs-right">
+    
+  <?php 
+      if(!EMPTY($_SESSION['id'])) { 
+  ?>
+        <a class="btn btn-primary btn-lg" href="?function=logout" style="margin-right:15px" > Logout</a>
+  
+</button>
+
+  <?php } else { ?>
+
+    
+ 
+  <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#login" style="margin-right:15px" > Login </button>  
+  <button type="button" class="btn btn-secondary btn-lg" data-toggle="modal" data-target="#signup" style="margin-right:20px"> Sign Up </button>
+
+    <?php }  ?>
+  </form>
+</nav>
+
+    <div class="centertext">
+        <h1>INTERACTIVE</h1>
+        <p>CLASSICAL ART</p>
+    </div>
+	
+	
+
+<div class="container"><div class="columns"><div class="column is-8 is-offset-2"><h3><span style="color: #333333;"><b>How Does the 3D Hologram Pyramid Work?</b></span></h3>
+The hologram pyramid is a simple device that can be made by manipulating a sheet of plastic into the shape of a pyramid with its top cut off. The device creates a 3D-like illusion for the viewer and makes an image or video appear as if it were in midair. It works on the principle of <a href="https://en.wikipedia.org/wiki/Pepper%27s_ghost" target="_blank">Pepper's Ghost</a>. Four symmetrically opposite variations of the same image are projected onto the four faces of the&nbsp;pyramid. By principle, each side projects the image falling on it to the center of the pyramid. These projections work in unison to form a whole figure, which creates a 3D illusion.
+<br>
+
+
+<h3><b style="color: rgb(51, 51, 51); font-style: inherit;"><br>Follow These Steps for Your Own Hologram Pyramid</b><br></h3>
+<p>Now this is very easy instructable to turn your smartphone into a small hologram. It takes roughly 10-15 minutes to make it.</p>
+<p>First of all you will need these materials to proceed:</p>
+	<ul><li>Acrylic plexiglass.</li><li>Graph or Plain Paper.</li><li>Pen.</li><li>Tape.</li><li>Scissor.</li><li>Cutter.</li><li>Smartphone.</li></ul></div>
+	
+
+
+1. Print out the template shown below on a sheet of A4-size paper.
+</div></div></div>
+
+<div class="container"><div class="columns"><div class="column is-8 is-offset-2"><div class="content-image"><div class="wrapper"><img src="https://maker.pro/storage/0ea1d8f/0ea1d8f477a94ad872879107936f1d8b.jpeg" alt="" style=""></div></div></div></div></div>
+
+<div class="container"><div class="columns"><div class="column is-8 is-offset-2"><p><strong>NOTE:</strong> If you do not have access to a printer, you can also make a template on your own. Draw a basic "Trapezoid" on a piece of paper using these dimensions. Parallel sides= 1cm, 6cm &amp; other two sides= 4.5cm each. You can always double or triple the dimensions in proportion to use on a larger display.
+<br><br>2. Trace out the shape on the plastic sheet using a ruler and pen. For the trapezoid template, trace out four similar outlines on the plastic sheet. Now carefully&nbsp;cut out the outlines with a cutting blade and ruler. Try to make your cuts as precise as possible for a perfect pyramid.
+<br><span style="font-weight: 400;"><br>3. </span><strong>If you used the printout template:</strong><span style="font-weight: 400;"> Score the red edges very lightly using a cutting blade. This will allow you to fold the edges better and form a pyramid shape.&nbsp;Stick the open edges of the sheet together using clear cellotape.
+<br></span><strong><br>If you used the trapezoid template:</strong><span style="font-weight: 400;"> Join the four edges to form a pyramid shape.&nbsp;Stick them together.
+<br><br>Either way, in the end you will have a pyramid like the one shown below.</span><br></p></div></div></div>
+
+<div class="container"><div class="columns"><div class="column is-8 is-offset-2"><div class="content-image"><div class="wrapper"><img src="https://maker.pro/storage/78216d4/78216d4b5a43de0fe88bdd70052ae4ed.jpeg" alt="" style=""></div></div></div></div></div>
+
+<div class="container"><div class="columns"><div class="column is-8 is-offset-2"><p>4. That's it! You have made yourself a hologram pyramid! All you have to do now is go into the holograms section and enjoy your interactive experience in your phone.&nbsp;Place the hologram pyramid in the middle&nbsp;of your screen, like shown in the image below, and enjoy the show. Don't forget to switch off the lights in the room before you start playing the video.</p></div></div></div>
+
+<div class="container"><div class="columns"><div class="column is-8 is-offset-2"><div class="content-image"><div class="wrapper"><img src="https://maker.pro/storage/4ec0c2b/4ec0c2b04b6a04ee0a4eccd692e03ce3.jpeg" alt="" style=""></div></div></div></div></div>
+</body>
+
+<footer class="footer">
+
+  
+      <p style="padding-left:50px">&copy; Interactive Classical Art 2020 <p>
+ 
+
+</footer>
+</html>
